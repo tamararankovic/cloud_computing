16f8745726f2396d97ac61f3d301872e204fc1ef
Tamara Ranković <rankovictamaraa@gmail.com>
2021-11-24 15:50:12 +0100

    endpoint for fetching all songs

--- app/webapp/song/serializers.py
+++ app/webapp/song/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Song
 
 
-class StudentSerializer(serializers.ModelSerializer):
+class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = ['id', 'title', 'performedBy', 'releaseDate', 'rating']
--- app/webapp/song/views.py
+++ app/webapp/song/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Song
+from .serializers import SongSerializer
+from django.http import JsonResponse, HttpResponse
 
-# Create your views here.
+def get_all(request):
+    songs = Song.objects.all()
+    serializer = SongSerializer(songs, many=True)
+    return JsonResponse(serializer.data, safe=False, status=200)
